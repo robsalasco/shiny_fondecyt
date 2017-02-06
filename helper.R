166eaf33cffa0ec3f0c85d8096c6d572d27c97bb
robsalasco <wala.rsalas@gmail.com>
2017-02-05 21:52:12 -0300

    fix

---
+++ helper.R
@@ -0,0 +1,12 @@
+library(rgdal)
+library(rmapshaper)
+
+chl <- readOGR(dsn="data/comunas.json",encoding = "UTF-8")
+chl <- ms_simplify(chl, keep = 0.1)
+write_rds(chl,"data/chl.rds")
+
+muni_data <- read_csv("data/piemunicipal.csv")
+write_rds(muni_data,"data/piemunicipal.rds")
+
+social_data <- read_csv("data/sociales.csv")
+write_rds(social_data,"data/sociales.rds")
